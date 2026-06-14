a9790a820cfb62fb25de326562b038d90ba72e62
Rohith <rohithbirwa03@gmail.com>
2026-06-14 13:21:18 +0530

    Add attachments and result files for user registration test failure
    - Added PNG attachments for screenshots during test execution.
    - Created JSON result file capturing details of the user registration test failure due to connection issues.
    - Included video and trace files for better debugging of the failed test case.

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,13 +1,13 @@
 import { defineConfig, devices } from '@playwright/test';
 
 export default defineConfig({
-  timeout: 30 * 1000, // 30000 ms (30 secs)
+  timeout: 30 * 1000,   //30000 ms(30 secs)
   testDir: './tests',
-  fullyParallel: true,
+  fullyParallel: false,
   //retries: process.env.CI ? 2 : 0,
-  retries: 1,
+  retries:1,
   //workers: process.env.CI ? 1 : undefined,
-  workers: 2,
+  workers: 1,
 
   reporter: [
     ['html'],
@@ -17,8 +17,6 @@ export default defineConfig({
   ],
 
   use: {
-    //baseURL: process.env.BASE_URL || 'http://localhost:3000',
-    //baseURL: 'https://tutorialsninja.com/demo/',
     trace: 'on-first-retry',
     screenshot: 'only-on-failure',
     video: 'retain-on-failure',
@@ -27,20 +25,24 @@ export default defineConfig({
     ignoreHTTPSErrors: true, // Ignore SSL errors if necessary
     permissions: ['geolocation'], // Set necessary permissions for geolocation-based tests
   },
-//grep: /@master/,
 
-projects: [
-  {
-    name: 'chromium',
-    use: { ...devices['Desktop Chrome'] },
-  },
-  /*{
-    name: 'firefox',
-    use: { ...devices['Desktop Firefox'] },
-  },
-  {
-    name: 'webkit',
-    use: { ...devices['Desktop Safari'] },
-  }*/
-],
+  //grep: /@master/,
+
+  projects: [
+   {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+    /*{
+      name: 'firefox',
+      use: { ...devices['Desktop Firefox'] },
+    },
+
+    {
+      name: 'webkit',
+      use: { ...devices['Desktop Safari'] },
+    } */
+  ],
+
+
 });
